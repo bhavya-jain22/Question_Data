5e1d612418d22ca12091ee07ffe59a614af27d79
bhavya-jain22 <bjbhavyajain982@gmail.com>
2026-04-02 13:00:23 +0530

    added Leetcode_Soln

---
+++ Leetcode_Soln/N_Queens.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+    unordered_map<int,bool>cols,left,right;
+    void solve(int n,vector<vector<string>>&ans,int row,vector<string> &board){
+        if(row==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(cols[col]==0 && left[row-col]==0 && right[row+col]==0){
+                board[row][col]='Q';
+                cols[col]=1;left[row-col]=1;right[row+col]=1;
+                solve(n,ans,row+1,board);
+                board[row][col]='.';
+                cols[col]=0;left[row-col]=0;right[row+col]=0;
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string> board(n, string(n, '.'));
+        solve(n,ans,0,board);
+        return ans;
+    }
+};
