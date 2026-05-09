e36e554cd9ef912a79b847a8bed151e508644d43
bhavya-jain22 <bjbhavyajain982@gmail.com>
2026-05-09 13:37:37 +0530

    Floyd Warshall

---
+++ GFG_Soln/Floyd_Warshall.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        int n = dist.size(), inf = 1e8;
+        for(int k=0; k<n; k++) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(dist[i][k] != inf && dist[k][j] != inf) dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+};
