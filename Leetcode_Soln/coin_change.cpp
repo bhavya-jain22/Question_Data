4061d956f3ac8f6f03d5fa7e76c2c8fbfd295bc1
bhavya-jain22 <bjbhavyajain982@gmail.com>
2026-08-08 12:27:24 +0530

    .

--- Leetcode_Soln/coin_change.cpp
+++ Leetcode_Soln/coin_change.cpp
@@ -2,52 +2,58 @@
 // https://leetcode.com/problems/coin-change/
 
 class Solution {
-public:
-    // ---my code ---
-    int coinChange(vector<int>& coins, int amount) {
-        if(!amount) return 0;
-        
-        sort(coins.begin(), coins.end());
+private:
 
-        int n = coins.size();
-        unordered_map< int, vector<int> > dp;
-        dp[0] = vector<int>(amount+2, -1);
-
-        for(int i=0; i<n; i++) {
-            dp[coins[i]] = vector<int>(amount+2, -1);
+int utils(vector<vector<int>>& dp, vector<int>& coins, int n, int amount) {
+    if(amount == 0) return 0;
+    // special return for base case;
+    if(n==0) return -3;
+    if(dp[n][amount] != -1) return dp[n][amount];
+    if(coins[n-1] <= amount) {
+        int take = 1 + utils(dp, coins, n, amount-coins[n-1]);
+        int not_take = utils(dp, coins, n-1, amount);
+        if(take > 0 && not_take>0) {
+            return dp[n][amount] =  min(take, not_take);
         }
-
-        for(int i=0; i<n; i++) {
-            int coin = coins[i];
-            for(int j=1; j<=amount; j++) {
-                if(j<coin) {
-                    if(i>0) dp[coin][j] = dp[coins[i-1]][j];
-                }
-                else if(j==coin) dp[coin][j] = 1;
-                else {
-                    int notUse = (i>0) ? dp[coins[i-1]][j] : -1;
-                    int use = (dp[coin][j-coin] != -1) ? dp[coin][j-coin] + 1 : -1;
-                    if(use == -1) dp[coin][j] = notUse;
-                    else if(notUse == -1) dp[coin][j] = use;
-                    else dp[coin][j] = min(use, notUse);
-                }
-            }
+        else if(take>0) {
+            return dp[n][amount] = take;
         }
-
-        return dp[coins[n-1]][amount];
+        else return dp[n][amount] = not_take;
     }
+    return dp[n][amount] = utils(dp, coins, n-1, amount);
+}
 
+int tabulation(vector<vector<int>>& dp, vector<int>& coins, int n, int amount) {
+    // lower priority base case first;
+    // if(i==0) ->
+    for(int j=0; j<=amount; j++) dp[0][j] = -3;
+    // higher priority base case later;
+    // if(j==0) ->
+    for(int i=0; i<=n; i++) dp[i][0] = 0;
 
-    // --- optimal solution ---
+    int take = 0, not_take = 0;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=amount; j++) {
+            take = 0, not_take = 0;
+            if(coins[i-1] <= j) {
+                take = 1+dp[i][j-coins[i-1]];
+            }
+            not_take = dp[i-1][j];
 
-    
-    // int coinChange(vector<int>& coins, int amount) {
-    //     vector<int> dp(amount + 1, INT_MAX);
-    //     dp[0] = 0;
-    //     for(int coin : coins)
-    //         for(int j = coin; j <= amount; j++)
-    //             if(dp[j - coin] != INT_MAX)
-    //                 dp[j] = min(dp[j], dp[j - coin] + 1);
-    //     return dp[amount] == INT_MAX ? -1 : dp[amount];
-    // }
+            if(take > 0 && not_take>0) dp[i][j] =  min(take, not_take);
+            else if(take>0) dp[i][j] = take;
+            else dp[i][j] = not_take;
+        }
+    }
+    return dp[n][amount];
+}
+
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        // int ans = utils(dp, coins, n, amount);
+        int ans = tabulation(dp, coins, n, amount);
+        return ans<0? -1: ans;
+    }
 };
